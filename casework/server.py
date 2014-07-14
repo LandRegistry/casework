023e17104fb246c8fe97d6c069d34531e0b1b0bd
treacle74 <ptrelease@gmail.com>
2014-07-14 15:28:42 +0100

    changes to take a new title detail.  Need to do mint next so the title detail is just returned to the browser atm.

--- casework/server.py
+++ casework/server.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from flask import request
 from casework import app, db
 from  models import Foo
 from flask.ext.assets import Environment
@@ -11,6 +12,17 @@ def asset_path_context_processor():
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+
+@app.route('/new_title/', methods=['POST'])
+def new_title():
+
+  if request.method == 'POST':
+    mint_string = request.form['titleNumber'] + ':' + request.form['titleJSON']
+    return mint_string
+
+
 
 #  Some useful headers to set to beef up the robustness of the app
 # https://www.owasp.org/index.php/List_of_useful_HTTP_headers
