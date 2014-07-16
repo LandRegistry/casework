be16e850376e4c40282c4025618c991e636e15bc
treacle74 <ptrelease@gmail.com>
2014-07-16 17:52:39 +0100

    moved secret key to run.sh

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ import os
 
 class Config(object):
     DEBUG = False
-    CSRF_ENABLED = True
-    SECRET_KEY = 'a very secret thing indeed TODO: Get this from the os.environ - i.e. generate and export one'
     MINT_URL = os.environ['MINT_URL']
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    CSRF_ENABLED = os.environ['CSRF_ENABLED']
+    SECRET_KEY = os.environ['SECRET_KEY']
 
 class TestConfig(DevelopmentConfig):
     TESTING = True
