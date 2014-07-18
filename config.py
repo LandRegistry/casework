a6d6f0f9be0bbc9bd3ac4d4c5b7f8b306091b5a7
Adam Shimali <adam.shimali@gmail.com>
2014-07-18 14:03:08 +0100

    Adjust config to so key required at base level and turn off csrf in dev

--- config.py
+++ config.py
@@ -3,12 +3,13 @@ import os
 class Config(object):
     DEBUG = False
     MINT_URL = os.environ['MINT_URL']
-
-class DevelopmentConfig(Config):
-    DEBUG = True
     CSRF_ENABLED = os.environ['CSRF_ENABLED']
     SECRET_KEY = os.environ['SECRET_KEY']
 
+class DevelopmentConfig(Config):
+    DEBUG = True
+    CSRF_ENABLED = False
+
 class TestConfig(DevelopmentConfig):
     TESTING = True
-    WTF_CSRF_ENABLED = False
+
