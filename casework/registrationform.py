8f2016bf73dbaef66c5974e353c8fb6591d25e8d
alanhugheslandregistry <alanhugheslandregistry@gmail.com>
2014-07-18 10:05:12 +0100

    Adding WTForm validation

--- casework/registrationform.py
+++ casework/registrationform.py
@@ -1,8 +1,45 @@
 import os
 from flask_wtf import Form
-from wtforms import TextField
+from wtforms import TextField, RadioField
 from wtforms.validators import DataRequired
 
 class RegistrationForm(Form):
-    titleNumber = TextField('titleNumber', validators=[DataRequired()])
-    
+
+    """
+    The names of the variables here MUST match the name attribute of the fields
+    in the index.html for WTForms to work
+    """
+    titleNumber = TextField('Title Number')
+
+    #Proprietors
+    firstName1 = TextField('firstName1', validators=[DataRequired()])
+    surname1 = TextField('surname1', validators=[DataRequired()])
+    firstName2 = TextField('firstName2')
+    surname2 = TextField('surname2')
+
+    # Property details
+    houseNumber = TextField('houseNumber', validators=[DataRequired()])
+    road = TextField('road', validators=[DataRequired()])
+    town = TextField('town', validators=[DataRequired()])
+    postcode = TextField('postcode', validators=[DataRequired()])
+
+    #Property Tenure and Class
+    propertyTenure = RadioField(
+      'propertyTenure',
+      choices=[
+        ('freehold','Freehold'),
+        ('leasehold','Leasehold')
+      ]
+    )
+    propertyClass = RadioField(
+      'propertyClass',
+      choices=[
+        ('absolute','Absolute'),
+        ('good','Good'),
+        ('qualified','Qualified'),
+        ('possesory','Possesory')
+      ]
+    )
+
+    #Price Paid
+    pricePaid = TextField('pricePaid', validators=[DataRequired()])
