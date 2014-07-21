56f10e557b3c91f8ddad11206873b69be2dbefd1
alanhugheslandregistry <alanhugheslandregistry@gmail.com>
2014-07-21 17:02:27 +0100

    Adding a link to the public property page

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config(object):
     MINT_URL = os.environ.get('MINT_URL')
     CSRF_ENABLED = os.environ.get('CSRF_ENABLED')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    PROPERTY_FRONTEND_URL = os.environ.get('PROPERTY_FRONTEND_URL')
 
 class DevelopmentConfig(Config):
     DEBUG = True
@@ -12,4 +13,4 @@ class DevelopmentConfig(Config):
 class TestConfig(DevelopmentConfig):
     TESTING = True
     MINT_URL = 'http://locahost:8005'
-
+    PROPERTY_FRONTEND_URL = 'http://localhost:8002'
--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 
 export SETTINGS='config.DevelopmentConfig'
 export MINT_URL='http://0.0.0.0:8001'
-export SECRET_KEY="<F\xab\xdd\x83\xbc\xaa\xcc\xda;1*\x17I\x8d\xf0{\x15\xcd\x89\xaeS$:"
+export PROPERTY_FRONTEND_URL='http://0.0.0.0:8002'
+export SECRET_KEY='<F\xab\xdd\x83\xbc\xaa\xcc\xda;1*\x17I\x8d\xf0{\x15\xcd\x89\xaeS$:'
 export CSRF_ENABLED=True
 
 python run_dev.py
