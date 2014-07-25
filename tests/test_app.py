811137587abe1afdffd00095089ea9ce4583c530
Richard Pope <richard@memespring.co.uk>
2014-07-25 13:38:18 +0100

    added tests for different casings

--- tests/test_app.py
+++ tests/test_app.py
@@ -21,7 +21,7 @@ class CaseworkTestCase(unittest.TestCase):
         self.client = app.test_client()
 
     def check_server(self):
-        
+
         rv = self.client.get('/registration')
         assert rv.status == '200 OK'
 
@@ -31,7 +31,7 @@ class CaseworkTestCase(unittest.TestCase):
         rv = self.client.get('/')
         assert rv.status == '200 OK'
 
-    def test_create_form(self):
+    def get_valid_create_form(self):
 
         with self.app.test_request_context():
 
@@ -46,12 +46,39 @@ class CaseworkTestCase(unittest.TestCase):
             form.house_number.data = '101'
             form.road.data = "Lake Washington Bldv E"
             form.town.data = "Seattle"
-            form.postcode.data = 'TE57 CD3'
+            form.postcode.data = 'SW1A1AA'
 
             form.property_tenure.data = "freehold"
             form.property_class.data = "absolute"
             form.price_paid.data = "1000000"
             form.extent.data = '{ "type": "Feature", "geometry": { "type": "Point", "coordinates": [125.6, 10.1] }, "properties": { "name": "Dinagat Islands" } }'
 
-            valid = form.validate()
-            assert valid
+            return form
+
+    def test_postcode_validation(self):
+        form = self.get_valid_create_form()
+        form.postcode.data = 'XXXXX'
+
+        valid = form.validate()
+        assert valid == False
+
+        form.postcode.data = 'sw1a1aa'
+        valid = form.validate()
+        assert valid == True
+
+        form.postcode.data = 'sw1a 1aa'
+        valid = form.validate()
+        assert valid == True
+
+        form.postcode.data = 'SW1A1AA'
+        valid = form.validate()
+        assert valid == True
+
+
+
+
+    def test_create_form(self):
+
+        form = self.get_valid_create_form()
+        valid = form.validate()
+        assert valid
