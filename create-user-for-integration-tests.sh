5579224355afeae6cafb044b18746438222291ce
mooreandrew <mooreandrewlr@gmail.com>
2014-08-04 17:22:31 +0100

    Changed workon vm

--- create-user-for-integration-tests.sh
+++ create-user-for-integration-tests.sh
@@ -2,7 +2,7 @@
 
 source /vagrant/script/dev-env-functions
 source ./environment.sh
-create_virtual_env "service-frontend"
+create_virtual_env "casework-frontend"
 python manage.py create_user --email=caseworker@example.org --password=dummypassword
 
 deactivate
