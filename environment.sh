c05342822555d00f59ae1d3311b0281e1a511208
Juan M Uys <opyate@gmail.com>
2014-08-08 11:11:20 +0100

    bring config in line with dev env domains

--- environment.sh
+++ environment.sh
@@ -1,6 +1,6 @@
 export SETTINGS='config.DevelopmentConfig'
 export MINT_URL='http://0.0.0.0:8001'
-export PROPERTY_FRONTEND_URL='http://0.0.0.0:8002'
+export PROPERTY_FRONTEND_URL='http://property-frontend.landregistry.local'
 export SECRET_KEY='local-dev-not-secret'
 export CSRF_ENABLED=False
 export DATABASE_URL='postgresql://localhost/casework_users'
