bb3eb3e3643486b231ca91ada218bb2321005923
Mat Wall <mat.wall@digital.cabinet-office.gov.uk>
2014-08-15 09:36:06 +0100

    Modified to echo out the user & password that have been created to make it easier for developers

--- create-user-for-integration-tests.sh
+++ create-user-for-integration-tests.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+USER_EMAIL="caseworker@example.org"
+USER_PASSWORD="dummypassword"
+
 source /vagrant/script/dev-env-functions
 source ./environment.sh
 create_virtual_env "casework-frontend"
-python manage.py create_user --email=caseworker@example.org --password=dummypassword
-
+python manage.py create_user --email=${USER_EMAIL} --password=${USER_PASSWORD}
 deactivate
+
+echo
+echo
+echo "Created user: ${USER_EMAIL} with password: ${USER_PASSWORD}"
+echo
+echo
