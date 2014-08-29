17e2222d399c48fae6e621f780fd3acb29c5d99b
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2014-08-29 17:51:11 +0100

    missed a merge tag

--- application/frontend/forms.py
+++ application/frontend/forms.py
@@ -12,8 +12,7 @@ import logging
 class MultiCheckboxField(SelectMultipleField):
     widget = widgets.ListWidget(prefix_label=False)
     option_widget = widgets.CheckboxInput()
->>>>>>> master
-
+    
 
 class ChargeForm(Form):
     """
