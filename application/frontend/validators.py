60f6ffe09a5fa47717066bf97c577d802cbe8d7e
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2014-08-29 18:05:00 +0100

    fix time convert method

--- application/frontend/validators.py
+++ application/frontend/validators.py
@@ -9,18 +9,17 @@ import logging
 
 
 class ValidateDateNotInFuture(object):
+    def __init__(self):
+        self.message = "The date must not be in the future"
+
     def __call__(self, form, date_field):
         if date_field.data > date.today():
             raise ValidationError('Date cannot be in the future')
 
-    def validate_date_not_in_future(form, date_field):
-        if date_field > date.today():
-            raise ValidationError('Date cannot be in the future')
-
-    def convert_to_bst(dt):
-        utc = timezone('UTC').localize(dt)
-        bst = timezone('Europe/London').localize(dt)
-        return bst + (utc - bst)
+def convert_to_bst(dt):
+    utc = timezone('UTC').localize(dt)
+    bst = timezone('Europe/London').localize(dt)
+    return bst + (utc - bst)
 
 
 class ValidateEasementWithinExtent(object):
