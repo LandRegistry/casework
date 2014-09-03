6d3b8cc88de51c605c3244e1f4585107b8208567
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2014-09-03 11:19:14 +0100

    Added missing tests for forms.

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -3,7 +3,7 @@ import unittest
 import json
 
 from application.frontend.frontend import app
-from application.frontend.forms import RegistrationForm, ChargeForm, LeaseholdForm
+from application.frontend.forms import RegistrationForm, ChargeForm, LeaseholdForm, EasementForm
 from geo_json_fixtures import valid_geo_json
 
 
@@ -13,7 +13,7 @@ class FormsTestCase(unittest.TestCase):
         self.app = app
         self.client = app.test_client()
 
-    def test_valid_create_form_with_charge(self):
+    def test_validate_form_is_true_when_form_is_valid(self):
         with self.app.test_request_context():
             form = RegistrationForm()
 
@@ -32,53 +32,19 @@ class FormsTestCase(unittest.TestCase):
             form.price_paid.data = "10000.00"
             form.price_paid.raw_data = "10000.00"
 
-            charge_form = ChargeForm()
-            charge_form.chargee_name.data = "Company 1"
-            charge_form.charge_date.data = datetime.datetime.strptime("01-02-2001", "%d-%m-%Y")
-            charge_form.chargee_address.data = "21 The Street Plymouth UK PL1 1AA"
-            charge_form.chargee_registration_number.data = "1234567"
-            charge_form.has_restriction = False
-            form.charges.raw_data = [charge_form]
-
             form.extent.data = json.dumps(valid_geo_json)
             self.assertTrue(form.validate())
 
-    def test_leasehold_is_true_and_leasehold_form_is_not_valid(self):
+    def test_validate_leasehold_form_contains_errors_for_all_missing_required_fields(self):
         with self.app.test_request_context():
-            form = RegistrationForm()
-
-            form.title_number.data = "TEST1234"
-            form.full_name1.data = "Kurt Cobain"
-            form.full_name2.data = "Courtney Love"
-
-            form.address_line_1.data = '101 Lake Washington Bldv E'
-            form.address_line_2.data = "line2"
-            form.address_line_3.data = "line3"
-            form.address_line_4.data = "line4"
-            form.city.data = "Seattle"
-            form.postcode.data = 'SW1A1AA'
-            form.country.data = 'GB'
-
-            form.property_tenure.data = "Leasehold"
-            form.property_class.data = "Absolute"
-            form.price_paid.data = "10000.00"
-            form.price_paid.raw_data = "10000.00"
-
             lease = LeaseholdForm()
-            lease.lease_date.data = None
-            lease.lease_term.data = None
-            lease.lease_from.data = None
-            form.leases.raw_data = [lease]
+            self.assertFalse(lease.validate())
 
-            form.extent.data = json.dumps(valid_geo_json)
-            self.assertFalse(form.validate())
-
-            # lease = form.leases[0]
-            # self.assertEquals(lease.lease_date.errors[0], 'This field is required.')
-            # self.assertEquals(lease.lease_term.errors[0], 'This field is required.')
-            # self.assertEquals(lease.lease_from.errors[0], 'This field is required.')
-            # self.assertEquals(lease.lessor_name.errors[0], 'This field is required.')
-            # self.assertEquals(lease.lessee_name.errors[0], 'This field is required.')
+            self.assertEquals(lease.lease_date.errors[0], 'This field is required.')
+            self.assertEquals(lease.lease_term.errors[0], 'This field is required.')
+            self.assertEquals(lease.lease_from.errors[0], 'This field is required.')
+            self.assertEquals(lease.lessor_name.errors[0], 'This field is required.')
+            self.assertEquals(lease.lessee_name.errors[0], 'This field is required.')
 
 
     def test_form_contains_errors_for_all_missing_required_fields(self):
@@ -148,9 +114,9 @@ class FormsTestCase(unittest.TestCase):
 
     def create_valid_leasehold_form(self):
         form = LeaseholdForm()
-        form.lease_date.data = datetime.date.today() - datetime.timedelta(days=1)
+        form.lease_date.data = datetime.date.today()
         form.lease_term.data = 10
-        form.lease_from.data = datetime.date.today() - datetime.timedelta(days=1)
+        form.lease_from.data = datetime.date.today()
         form.lessee_name.data = 'Bob Jones'
         form.lessor_name.data = 'Jo Blow'
         form.lease_easements.data = True
@@ -183,9 +149,55 @@ class FormsTestCase(unittest.TestCase):
     def test_leasehold_form_lease_date_is_not_in_the_future(self):
         with self.app.test_request_context():
             form = self.create_valid_leasehold_form()
-            form.lease_date.data =  datetime.date.today() + datetime.timedelta(days=1)
+            form.lease_date.data =  (datetime.datetime.today() + datetime.timedelta(days=1)).date()
             self.assertFalse(form.validate())
             self.assertEquals(form.lease_date.errors[0], "Date cannot be in the future")
 
             form.lease_date.data =  datetime.date.today()
             self.assertTrue(form.validate())
+
+    def test_charge_form_is_valid(self):
+        with self.app.test_request_context():
+            charge_form = ChargeForm()
+            charge_form.chargee_name.data = "Company 1"
+            charge_form.charge_date.data = datetime.date.today()
+            charge_form.chargee_address.data = "21 The Street Plymouth UK PL1 1AA"
+            charge_form.chargee_registration_number.data = "1234567"
+            charge_form.has_restriction.data = False
+
+            self.assertTrue(charge_form.validate())
+
+    def test_validate_charge_form_contains_errors_for_all_missing_required_fields(self):
+        with self.app.test_request_context():
+            charge_form = ChargeForm()
+            self.assertFalse(charge_form.validate())
+
+            self.assertEquals(charge_form.charge_date.errors[0], 'This field is required.')
+            self.assertEquals(charge_form.chargee_name.errors[0], 'This field is required.')
+            self.assertEquals(charge_form.chargee_address.errors[0], 'This field is required.')
+            self.assertEquals(charge_form.chargee_registration_number.errors[0], 'This field is required.')
+
+    def test_validate_easement_form_is_true_when_form_is_valid(self):
+        with self.app.test_request_context():
+            easement = EasementForm()
+            easement.easement_description.data = "some descr"
+            easement.easement_geometry.data = json.dumps(valid_geo_json)
+            self.assertTrue(easement.validate())
+
+    def test_validate_easement_form_contains_errors_for_all_missing_required_fields(self):
+        with self.app.test_request_context():
+            easement = EasementForm()
+            self.assertFalse(easement.validate())
+
+            self.assertEquals(easement.easement_description.errors[0], 'This field is required.')
+            self.assertEquals(easement.easement_geometry.errors[0], 'This field is required.')
+
+    def test_validate_easement_form_contains_errors_when_geojson_is_not_valid(self):
+        with self.app.test_request_context():
+            easement = EasementForm()
+            easement.easement_description.data = 'some description'
+            easement.easement_geometry.data = '{"type": "Feature","geometry": {"type": "Point","coordinates": [125.6, 10.1]},"properties": {"name": "Dinagat Islands"}}'
+
+            self.assertFalse(easement.validate())
+            self.assertEquals(easement.easement_geometry.errors[0], 'Valid GeoJSON is required')
+            self.assertEquals(easement.easement_description.errors, [])
