9bf0f55949e4cc96c3ff4ec6a38aced125e86c31
Juan Matthys Uys <opyate@gmail.com>
2014-09-30 13:35:52 +0100

    use postgresql for testing

--- environment_test.sh
+++ environment_test.sh
@@ -1,7 +1,14 @@
+# Here we'll create a test database, and override the database to the test values.
+export APP_NAME="casework_frontend_test"
+set +o errexit
+createuser -s $APP_NAME
+createdb -U $APP_NAME -O $APP_NAME $APP_NAME -T template0
+set -e
+
+export DATABASE_URL="postgresql://localhost/$APP_NAME"
 export SETTINGS='config.TestConfig'
 export MINT_URL='http://0.0.0.0:8001'
 export PROPERTY_FRONTEND_URL='http://0.0.0.0:8002'
 export SECRET_KEY='local-dev-not-secret'
 export CSRF_ENABLED=False
-export DATABASE_URL='sqlite://'
 export CASES_URL='http://nowhere'
