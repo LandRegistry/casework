4f5df385afe9f1ca3992c0f5fb208066a92d3ba9
Juan Matthys Uys <opyate@gmail.com>
2014-09-30 13:50:40 +0100

    remove APP_NAME var

--- environment_test.sh
+++ environment_test.sh
@@ -1,11 +1,10 @@
 # Here we'll create a test database, and override the database to the test values.
-export APP_NAME="casework_frontend_test"
 set +o errexit
-createuser -s $APP_NAME
-createdb -U $APP_NAME -O $APP_NAME $APP_NAME -T template0
+createuser -s casework_frontend_test
+createdb -U casework_frontend_test -O casework_frontend_test casework_frontend_test -T template0
 set -e
 
-export DATABASE_URL="postgresql://localhost/$APP_NAME"
+export DATABASE_URL="postgresql://localhost/casework_frontend_test"
 export SETTINGS='config.TestConfig'
 export MINT_URL='http://0.0.0.0:8001'
 export PROPERTY_FRONTEND_URL='http://0.0.0.0:8002'
